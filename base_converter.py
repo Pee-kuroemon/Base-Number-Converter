d15636a2ab8bc0866e3131673b2ff2316eac21de
Natthawee <s7070036@kmitl.ac.th>
2014-12-07 16:34:47 +0700

    change font and add result frame

--- base_converter.py
+++ base_converter.py
@@ -53,9 +53,7 @@ def ctrl():
     if switch == 0:
         result = "Please Try again! It's ERROR!"
     else:
-        if cls.num == []:
-            result = "Please Insert Number!"
-        elif cls.base_source == cls.base_target:
+        if cls.base_source == cls.base_target:
             result = "The Result is " + ("".join(cls.num))
         elif cls.base_source == 10:
             cls.dec = cls.num
@@ -77,39 +75,39 @@ base = {2:2, 3:3, 4:4, 5:5, 6:6, 7:7, 8:8, 9:9, \
         10:10, 11:11, 12:12, 13:13, 14:14, 15:15, 16:16}
 #title
 root.title("Base Number Converter")
-root.geometry("%sx%s+%s+%s"%(250, 150, 0, 0))
-root.resizable(0, 0)
 #Convert from
 frame_1 = Frame(root)
 frame_1.pack()
-box_1 = LabelFrame(frame_1, text="Input Number", padx=3, pady=3, font="times")
+box_1 = LabelFrame(frame_1, text="Input Number", padx=3, pady=3)
 box_1.pack(anchor=CENTER)
-box_1_pack = Entry(box_1, bd = 3, width = 20)
+box_1_pack = Entry(box_1, bd = 3, width = 30)
 box_1_pack.pack()
 #change conversion
 frame_2 = Frame(root)
 frame_2.pack()
-label_1 = Label(frame_1, text="From base", font="times")
+label_1 = Label(frame_1, text="From base")
 label_1.pack(side=LEFT)
 var_1 = IntVar(frame_1)
 base_1 = OptionMenu(frame_1, var_1, *base)
 var_1.set(10)
 base_1.pack(side=LEFT)
-label_2 = Label(frame_1, text="to base", font="times")
+label_2 = Label(frame_1, text="to base")
 label_2.pack(side=LEFT)
 var_2 = IntVar(frame_1)
 base_2 = OptionMenu(frame_1, var_2, *base)
 var_2.set(10)
 base_2.pack(side=LEFT)
 #calculate button
-frame_3 = Frame(root)
+frame_3 = Frame(root, bd=5)
 frame_3.pack()
-cal = Button(frame_3, text="Calculate", font="times", command = ctrl)
+cal = Button(frame_3, text="Calculate", command = ctrl)
 cal.pack()
 #result
-frame_4 = Frame(root)
+frame_4 = Frame(root, bd=5)
 frame_4.pack()
-label = Label(root, font="times")
+label_a = LabelFrame(root, text="Result", padx=3, pady=3)
+label_a.pack()
+label = Label(label_a)
 label.pack()
 #mainloop
 root.mainloop()
